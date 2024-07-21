b0166ecdd3c9f11184432d8ec7041b59858d1010
marinkres <131777129+marinkres@users.noreply.github.com>
2024-07-21 20:01:00 +0200

    test

--- astro.config.ts
+++ astro.config.ts
@@ -45,7 +45,7 @@ export default defineConfig({
 	// https://docs.astro.build/en/guides/prefetch/
 	prefetch: true,
 	// ! Please remember to replace the following site property with your own domain
-	site: "https://pluggy.netlify.app/",
+	site: "https://marink.me/",
 	vite: {
 		optimizeDeps: {
 			exclude: ["@resvg/resvg-js"],
