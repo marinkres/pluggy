d7867b718731e0ab8761777d623fc3c136259db8
marinkres <131777129+marinkres@users.noreply.github.com>
2024-07-23 20:26:35 +0200

    webmention

--- src/site.config.ts
+++ src/site.config.ts
@@ -25,7 +25,7 @@ export const siteConfig: SiteConfig = {
 	title: "Marink",
 	webmentions: {
 		// Webmention.io API endpoint. Get your own here: https://webmention.io/, and follow this blog post: https://astro-cactus.chriswilliams.dev/posts/webmentions/
-		link: "https://webmention.io/marink.me/webmention",
+		link: "https://webmention.io/api/mentions.html?token=xdK-5dTEUoIWA8_j54v6lw",
 	},
 };
 
